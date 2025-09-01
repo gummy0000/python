0dae69b103088d828a7c41f26403ab14cd6c330d
gummy0000 <kky06288@gmail.com>
2025-09-01 12:48:53 +0900

    Add files via upload

---
+++ index.py
@@ -0,0 +1,12 @@
+a = 'Life is too short, You need Python'
+print(len(a))
+
+print(a[5]) # i
+print(a[19]) # Y
+print(a[0]+a[19]+a[28])
+print(a[12:17])
+print(a[23:27])
+print(a[8:17])
+print(a[12:17])
+print(a[19:-7])
+print(a[18:-7])
